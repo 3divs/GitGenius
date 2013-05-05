d2cb11d4559c6cec692681eb26fa6accfa94772d
Andrew Magliozzi <andrewmagliozzi@Andrew-Magliozzis-MacBook-Air.local>
2013-05-04 20:03:10 -0400

    added some stock ajax code to this file

--- GitGenius.js
+++ GitGenius.js
@@ -1,13 +1,34 @@
+var cred = 'client_id=eb9827e7a7daab7678ce&client_secret=b596b7ca0af0f554fe396e81cd647ebe4b0ebb4e';
+// ''+'?'+cred
+
+var requestHeader = {
+  "accept": "application/vnd.github.raw",
+  "content-type": "application/JSONP",
+  "authorization": 'token b596b7ca0af0f554fe396e81cd647ebe4b0ebb4e',  
+  "host": 'api.github.com'
+};
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to GitGenius.";
   };
 
 Meteor.loginWithGithub({
-  requestPermissions: ['user', 'repo']
+  requestPermissions: ['user', 'public_repo', 'repo']
 }, function (err) {
   if (err)
     Session.set('errorMessage', err.reason || 'Unknown error');
+});
+
+$.ajax({
+    url: 'https://api.github.com/orgs/3divs' + '?'+ cred,
+    dataType: 'JSONP',
+    success: function(){
+            alert('hooray!');
+    },
+    error: function(data){
+            console.log('sad', data);
+    }
 });
 
   Template.hello.events({
@@ -24,3 +45,4 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
