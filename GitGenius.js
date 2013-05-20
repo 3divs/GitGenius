e51c03682de0a8e7872793dfd0e0f867db9a27bd
Andrew Magliozzi <andrewmagliozzi@Andrew-Magliozzis-MacBook-Air.local>
2013-05-20 00:58:03 -0400

    refactored ajax calls with meteor futures.  No crashes but not yet confirmed that they write to the db properly.

--- GitGenius.js
+++ GitGenius.js
@@ -17,24 +17,29 @@ if (Meteor.isClient) {
   Template.menu_bar.events = {
 
     'click .addarepo': function() {
-        Meteor.Router.to('/addarepo');
+      Meteor.Router.to('/addarepo');
     },
 
     'click .underscoreDir': function() {
-        Meteor.Router.to('/underscoreDir');
+      Meteor.Router.to('/underscoreDir');
     },
 
     'click .about': function() {
-    Meteor.Router.to('/about');
+      Meteor.Router.to('/about');
     },
 
     'click .contact': function() {
-    Meteor.Router.to('/contact');
+      Meteor.Router.to('/contact');
     },
 
     'click .home': function() {
-    Meteor.Router.to('/home');
-    }
+      Meteor.Router.to('/home');
+    },
+
+    'click .input-block-level': function(){
+      var url = $('.input-block-level').val()
+      Meteor.getFilesForRepo(url);
+    } 
   };
 
   Template.home.events = {
@@ -63,6 +68,142 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    var GitHubAPI = 'https://api.github.com',
+    cred = 'client_id=eb9827e7a7daab7678ce&client_secret=b596b7ca0af0f554fe396e81cd647ebe4b0ebb4e';
+
+    // call this method upon github repo URL submission to get all 
+    Meteor.getFilesForRepo = function (url){
+      var link = url.split('/'),
+      repo = link[link.length - 1],
+      user = link[link.length - 2],
+      fut = new Future();
+
+      Meteor.http.get(GitHubAPI + '/repos/' + user + '/' + repo + '/contents?recursive=1?' + cred, {
+        contentType: 'application/json',
+        dataType: 'jsonp'
+        }, function (err, res) {
+          if (err) throw 'failed callback';
+        });
+        var result = fut.wait();
+        if (result.statusCode === 200) {
+          var somedata = result.data;
+          console.log('some data', somedata);
+          for (var i = 0; i < somedata.length; i++) {
+            var tempURL = somedata[i].git_url;
+            // console.log('url', tempURL);
+            Files.findAndModify({
+              query : {
+                'url' : tempURL
+              },
+              update : {
+                'repository' : repo,
+                'repositoryOwner' : user,
+                'fileName' : somedata[i].name,
+                'filePath' : somedata[i].path,
+                'fileType' : somedata[i].type,
+                'url' : tempURL,
+                'sha' : somedata[i].sha,
+                'dateUploaded' : new Date()
+              },
+              upsert: true
+            });
+          };
+        } else {
+          console.log('trouble getting repo list');
+        };
+    };
+
+    // call this function any time a user clicks a link to view one of our files
+    Meteor.getFileContents = function (url){
+      var bitContent,
+      fut = new Future();
+
+      Meteor.http.get(fileURL + "?" + cred, {
+          contentType: 'application/json',
+          dataType: 'jsonp'
+        }, function (err, res) {
+          if (err) throw 'failed callback';
+      });
+
+      var result = fut.wait();
+      if (result.statusCode === 200) {
+        bitContent = results.data.content;
+        // console.log('bitContent', bitContent);
+        var plainContent = decode64(bitContent);
+        // console.log(plainContent);
+        console.log('results', results);
+        return plainContent;          
+      } else {
+        console.log('error getting file');
+      }
+    };
+
+      var _utf8_decode = function (utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+
+        while ( i < utftext.length ) {
+
+          c = utftext.charCodeAt(i);
+
+          if (c < 128) {
+            string += String.fromCharCode(c);
+            i++;
+          }
+          else if((c > 191) && (c < 224)) {
+            c2 = utftext.charCodeAt(i+1);
+            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            i += 2;
+          }
+          else {
+            c2 = utftext.charCodeAt(i+1);
+            c3 = utftext.charCodeAt(i+2);
+            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            i += 3;
+          }
+
+        }
+        
+        return string;
+      };
+
+      var decode64 = function (input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+        var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        while (i < input.length) {
+
+          enc1 = _keyStr.indexOf(input.charAt(i++));
+          enc2 = _keyStr.indexOf(input.charAt(i++));
+          enc3 = _keyStr.indexOf(input.charAt(i++));
+          enc4 = _keyStr.indexOf(input.charAt(i++));
+
+          chr1 = (enc1 << 2) | (enc2 >> 4);
+          chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+          chr3 = ((enc3 & 3) << 6) | enc4;
+
+          output = output + String.fromCharCode(chr1);
+
+          if (enc3 != 64) {
+            output = output + String.fromCharCode(chr2);
+          }
+          if (enc4 != 64) {
+            output = output + String.fromCharCode(chr3);
+          }
+
+        }
+
+        output = _utf8_decode(output);
+
+        return output;
+
+      };
   });
 }
+
